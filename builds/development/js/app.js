8bf976c36d25ce122cf6a12e141706298c8b60b7
Hongbo Jing <jack.jing7@gmail.com>
2015-09-07 02:47:04 -0400

    error, I do not know what happened

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -2,18 +2,13 @@ angular
     .module('myApp', ['ngRoute'])
     .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
         $routeProvider
-            .when('/',{
-                templateUrl:'partials/default.html'
-            })
             .when('/sample1',{
                 templateUrl:'partials/sample1.html'
             })
             .when('/sample2', {
                 templateUrl:'partials/sample2.html'
             })
-            .otherwise({
-                redirectTo:'/'
-            });
+            ;
             $locationProvider.html5Mode(true);
 
     }]);
