608a79d77e4267683b18b635914bb616c7f7b1b5
Hongbo Jing <jack.jing7@gmail.com>
2015-09-07 12:25:53 -0400

    day 71

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -1,14 +1,21 @@
 angular
     .module('myApp', ['ngRoute'])
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider
-            .when('/sample1',{
-                templateUrl:'partials/sample1.html'
+            .when('/', {templateUrl: 'partials/default.html'})
+            .when('/sample1', {
+                templateUrl: 'partials/sample1.html'
+            })
+            .when('/testParam/:itemId', {
+                templateUrl: 'partials/testParam.html',
+                controller: 'ParamController',
+                controllerAs: 'ParamCtrl'
             })
             .when('/sample2', {
-                templateUrl:'partials/sample2.html'
+                templateUrl: 'partials/sample2.html'
             })
-            ;
-            $locationProvider.html5Mode(true);
+            .otherwise({redirectTo: '/'})
+        ;
+        $locationProvider.html5Mode(true);
 
     }]);
